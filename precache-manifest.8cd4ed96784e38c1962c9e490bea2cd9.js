cafa2a33cea8de0175588ee5074db236d6267b18
elenapushenko <helenekisselova@gmail.com>
2020-05-01 16:48:59 +0300

    Updates

---
+++ precache-manifest.8cd4ed96784e38c1962c9e490bea2cd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31bcb61bbe1ea74277188292805427ab",
+    "url": "/friday-project/index.html"
+  },
+  {
+    "revision": "b272e8cdd43fa21382ff",
+    "url": "/friday-project/static/css/main.d3aaa3ee.chunk.css"
+  },
+  {
+    "revision": "cb29deafd5edf45bc2ab",
+    "url": "/friday-project/static/js/2.b3a1efc0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/friday-project/static/js/2.b3a1efc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b272e8cdd43fa21382ff",
+    "url": "/friday-project/static/js/main.8d902a1f.chunk.js"
+  },
+  {
+    "revision": "bb2cbc086f818df367fb",
+    "url": "/friday-project/static/js/runtime-main.e21d784b.js"
+  }
+]);
